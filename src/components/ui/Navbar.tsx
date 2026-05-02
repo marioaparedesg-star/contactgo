487fe47a6d1fb886c678ab31f88a9f2d45197a05
Mario A. Paredes <gerenntedetienda@Gerenntes-MacBook-Pro.local>
2026-05-02 19:11:29 -0400

    Fix Image import alias in Navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 h-16 flex items-center justify-between gap-4">
         {/* Logo */}
         <Link href="/" className="flex items-center">
-          <Image src="/logo.png" alt="ContactGo" width={140} height={50} className="h-10 w-auto object-contain" priority />
+          <NextImage src="/logo.png" alt="ContactGo" width={140} height={50} className="h-10 w-auto object-contain" priority />
         </Link>
 
         {/* Nav desktop */}
