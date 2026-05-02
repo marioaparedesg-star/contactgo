4891f54d72d94a20db0e5b6b89391d1382103b00
Mario A. Paredes <gerenntedetienda@Gerenntes-MacBook-Pro.local>
2026-05-02 13:24:29 -0400

    Remove WhatsApp line

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -113,7 +113,6 @@ export default function CheckoutPage() {
     clearCart()
     setLoading(false)
     router.push(order.id)
-    window.open('https://wa.me/18294089097?text=' + wamsg, '_blank')
     toast.success('¡Pedido confirmado! 🎉')
   }
 
