4aaf0d792bb40ed90712260955c19703e62a8f2d
Mario A. Paredes <gerenntedetienda@Gerenntes-MacBook-Pro.local>
2026-05-20 21:06:19 -0400

    feat: Sentry configurado via wizard oficial v6.12.0

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,19 @@
-import * as Sentry from "@sentry/nextjs"
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://abd80d1a5156109a90e030c28d221f2a@o4511406238203904.ingest.us.sentry.io/4511425062043648",
-  tracesSampleRate: 0.1,
-  enabled: process.env.NODE_ENV === "production",
-})
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
